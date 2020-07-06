a2178a8d2dcd13bcd93a8704a09aeadac54c9fe7
Matt Oatley <Matthew.Oatley225@gmail.com>
2020-07-06 11:22:26 -0500

    Add project files.

---
+++ StaticExcersice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExcersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelcius(60);
+
+
+            Console.WriteLine($"Celcius {celcius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(50);
+
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+
+        }
+    }
+}
---
+++ StaticExcersice/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExcersice
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            var result = (fahrenheit -32) /1.8;
+
+            return result; 
+        }
+
+        public static double CelsiusToFahrenheit(double celcius)
+        {
+            var result = (celcius - 1.8) / 32;
+
+            return result;
+        }
+
+    }
+}
